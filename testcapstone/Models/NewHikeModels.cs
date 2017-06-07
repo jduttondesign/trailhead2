f071f394a16bfe018fe517c532e8d2916389e3ff
Jennifer Dutton <jduttondesign@gmail.com>
2017-06-07 00:34:40 -0500

    NewHikeModels updated

--- testcapstone/Models/NewHikeModels.cs
+++ testcapstone/Models/NewHikeModels.cs
@@ -4,18 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 
 namespace testcapstone.Models
 {
     public class NewHike
     {
         [Key]
-        public int Id { get; set; }
+        public string Trail { get; set; }
 
-        public DateTime Date { get; set; }
+        public int Miles { get; set; }
 
-        public int NumberOfPeople { get; set; }
+        public string Terrain { get; set; }
 
-        public virtual NewHike DuckIAmEating { get; set; }
+        public string Safety { get; set; }
+    }
+
+    public class NewHikeDBContext : DbContext
+
+    {
+        public DbSet<NewHike> Trailheadtest { get; set; }
     }
 }
